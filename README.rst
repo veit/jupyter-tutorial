b2092af261e3bc9da2d1717fbd83d77f04241521
veit <kontakt@veit-schiele.de>
2019-08-01 08:39:24 +0200

    Add readme, license and setup script

---
+++ README.rst
@@ -0,0 +1,42 @@
+Jupyter-Tutorial
+================
+
+* Dokumentation: https://jupyter-tutorial.readthedocs.io/en/latest/
+* GitHub-Repo: https://github.com/veit/jupyter-tutorial/
+* Lizenz: BSD license
+
+Installation
+------------
+
+#. Herunterladen und Auspacken:
+
+    $ curl -O https://github.com/veit/jupyter-tutorial/archive/master.zip
+    $ unzip master.zip
+
+#. Sphinx installieren:
+
+    $ cd jupyter-tutorial
+    $ pipenv install
+
+#. HTML-Dokumentation erstellen:
+
+    $ pipenv run sphinx-build -b html docs/ docs/_build/
+
+#. PDF erstellen:
+
+    $ pipenv shell
+    Launching subshell in virtual environment…
+    $ bash-3.2$  . /Users/veit/.local/share/virtualenvs/jupyter-tutorial--q5BvmfG/bin/activate
+    $ (jupyter-tutorial) bash-3.2$ cd docs/
+    $ (jupyter-tutorial) bash-3.2$ make latexpdf
+
+   Das PDF findet ihr anschließend in ``docs/_build/latex/jupytertutorial.pdf``.
+
+Pull-Requests
+-------------
+
+Wenn ihr Vorschläge für Verbesserungen und Ergänzungen habt, empfehle ich euch,
+mit einen *Fork* eure eigene Version zu erstellen. Ich akzeptiere auch
+Pull-Requests, wenn diese klein und atomar sind sowie meine eigene Erfahrung
+verbessern.
+
---
+++ setup.py
@@ -0,0 +1,82 @@
+# !/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name='jupyter-tutorial',
+    packages=[],
+    version='0.1.0',
+    description='Jupyter Tutorial',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author='Veit Schiele',
+    license='BSD',
+    author_email='veit@cusy.io',
+    url='https://github.com/veit/cookiecutter-namespace-template',
+    keywords=['jupyter', 'ipython',],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: German',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: IPython',
+        'Framework :: Jupyter',
+        'Topic :: Desktop Environment',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development',
+    ],
+    install_requires=[
+        'sphinx',
+        'jupyter',
+        'nbsphinx',
+        'pandas',
+        'matplotlib',
+        'cython',
+        'memory-profiler',
+        'pytest',
+        'ipytest',
+        'ipython-unittest',
+        'jupyter-contrib-nbextensions',
+        'yapf',
+        'jupyter-latex-envs',
+        'bqplot',
+        'pythreejs',
+        'ipyvolume',
+        'ipyleaflet',
+        'networkx',
+        'autopep8',
+        'numpy',
+        'plotly<4',
+        'cufflinks',
+        'ipysheet',
+    ],
+)
+
+if any(bdist in sys.argv for bdist in ['bdist_wheel', 'bdist_egg']):
+    import setuptools
+
+if __name__ == '__main__':
+    setup(**setup_args)
+
