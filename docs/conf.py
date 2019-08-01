9370d095a167e8238c240b8004c732cb03e06217
veit <veit@cia.local>
2019-08-01 09:28:58 +0200

    Add ReadtheDocs config

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Veit Schiele'
 author = 'Veit Schiele'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
