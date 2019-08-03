42cdffbbbfbd9b051360d4ec15ca5fc788c1df53
veit <kontakt@veit-schiele.de>
2019-08-03 13:52:45 +0200

    Change title

--- docs/conf.py
+++ docs/conf.py
@@ -62,8 +62,10 @@ exclude_patterns = [
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = 'alabaster'
+
+# Change default HTML title
+html_title = 'Jupyter Tutorial v0.1.3'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
