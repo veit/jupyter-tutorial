f3adc0b8dad5ad3bb52da5358041a7a20b876343
veit <kontakt@veit-schiele.de>
2019-08-08 22:21:02 +0200

    Update to v0.1.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.rst", "r") as fh:
 setup_args = dict(
     name='jupyter-tutorial',
     packages=[],
-    version='0.1.7',
+    version='0.1.8',
     description='Jupyter Tutorial',
     long_description=long_description,
     long_description_content_type="text/x-rst",
