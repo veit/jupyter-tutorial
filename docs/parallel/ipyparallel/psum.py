d4f3b5ee4965b00d8d613585b70e813d74a542eb
veit <kontakt@veit-schiele.de>
2019-10-08 21:20:59 +0200

    Update to v0.3.2

---
+++ docs/parallel/ipyparallel/psum.py
@@ -0,0 +1,10 @@
+from mpi4py import MPI
+import numpy as np
+
+def psum(a):
+    locsum = np.sum(a)
+    rcvBuf = np.array(0.0,'d')
+    MPI.COMM_WORLD.Allreduce([locsum, MPI.DOUBLE],
+        [rcvBuf, MPI.DOUBLE],
+        op=MPI.SUM)
+    return rcvBuf
