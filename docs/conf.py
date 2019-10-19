ced3c96b461e6048d3a2254d344b54c13b7832e5
veit <kontakt@veit-schiele.de>
2019-10-19 17:01:40 +0200

    Update to v0.3.6

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Veit Schiele'
 author = 'Veit Schiele'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.5'
+release = '0.3.6'
 
 
 # -- General configuration ---------------------------------------------------
@@ -90,7 +90,7 @@ html_sidebars = {
 }
 
 # Change default HTML title
-html_title = 'Jupyter Tutorial v0.3.5'
+html_title = 'Jupyter Tutorial v0.3.6'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
