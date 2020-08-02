afcbc4b26007e7f60cea978507d91ca56b27e44c
veit <kontakt@veit-schiele.de>
2020-08-02 13:41:48 +0200

    Update black config

--- docs/refactoring/parallel/ipyparallel/psum.py
+++ docs/refactoring/parallel/ipyparallel/psum.py
@@ -5,5 +5,7 @@ import numpy as np
 def psum(a):
     locsum = np.sum(a)
     rcvBuf = np.array(0.0, "d")
-    MPI.COMM_WORLD.Allreduce([locsum, MPI.DOUBLE], [rcvBuf, MPI.DOUBLE], op=MPI.SUM)
+    MPI.COMM_WORLD.Allreduce(
+        [locsum, MPI.DOUBLE], [rcvBuf, MPI.DOUBLE], op=MPI.SUM
+    )
     return rcvBuf
